2ab985a8b901ebb7aeb42a3f35769104c2601a51
Srinivas Golusula <gsrinivasyadav2000@gmail.com>
2026-04-09 22:44:03 +0530

    2nd commit

--- src/components/DemoCarousel.jsx
+++ src/components/DemoCarousel.jsx
@@ -26,14 +26,54 @@ const slides = [
 
 function DemoCarousel(props) {
   return (
-    <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
+    <Carousel
+      autoPlay
+      infiniteLoop
+      showThumbs={false}
+      showStatus={false}
+      swipeable={false}
+      emulateTouch={true}
+    >
       {slides.map((slide, index) => (
-        // Add these classNames to your existing component structure
-        <div className="carousel-slide-container" key={index} id={props.id}>
+        <div
+          className="carousel-slide-container"
+          key={index}
+          id={props.id}
+          style={{ position: "relative" }}
+        >
           <img src={slide.image} alt={slide.title} className="carousel-image" />
-
-          <div className="carousel-overlay" height="100%" width="100%">
-            <div className="carousel-content">
+          {/* Transparent overlay */}
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              background: "rgba(0,0,0,0.3)",
+              zIndex: 1,
+            }}
+          />
+          <div
+            className="carousel-overlay"
+            height="100%"
+            width="100%"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              className="carousel-content"
+              style={{ position: "relative", zIndex: 3 }}
+            >
               <h1 className="carousel-title">{slide.title}</h1>
               <p className="carousel-subtitle">{slide.sub}</p>
               <div className="button-group">
